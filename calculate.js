c4cf794854445c7e506e095cb0754d4e325c6cbb
Vũ Văn Nghĩa <vunghiaxoan7@gmail.com>
2022-06-23 12:21:56 +0700

    chưa xong

--- calculate.js
+++ calculate.js
@@ -1,45 +1,138 @@
-let outputScreen = document.getElementById("output-screen");
+const container = document.querySelector(".calculator");
+const display = document.querySelector(".operator-display");
+const result = document.querySelector(".result");
+const keys = document.querySelector(".calculator-keys");
 
-let isNew = true;
-// for output
-// hiển thị
-function display(num) {
-  if (isNew) {
-    outputScreen.value += num;
-  } else {
-    outputScreen.value = "";
-    outputScreen.value += num;
-  }
-}
+const calculator_buttons = [
+  {
+    name: "clear",
+    label: "AC",
+    formula: false,
+    type: "key",
+    class: "clear-button",
+  },
+  {
+    name: "delete",
+    label: "CE",
+    formula: false,
+    type: "key",
+    class: "clear-button",
+  },
+  {
+    name: "percentage",
+    label: "%",
+    formula: "/100",
+    type: "key",
+    class: "key-operator",
+  },
+  {
+    name: "divide",
+    label: "/",
+    formula: "/",
+    type: "operator",
+    class: "key-operator",
+  },
+  {
+    name: "7",
+    label: 7,
+    formula: 7,
+    type: "number",
+  },
+  {
+    name: "8",
+    label: 8,
+    formula: 8,
+    type: "number",
+  },
+  {
+    name: "9",
+    label: 9,
+    formula: 9,
+    type: "number",
+  },
+  {
+    name: "multiple",
+    label: "x",
+    formula: "*",
+    type: "operator",
+    class: "key-operator",
+  },
+  {
+    name: "4",
+    label: 4,
+    formula: 4,
+    type: "number",
+  },
+  {
+    name: "5",
+    label: 5,
+    formula: 5,
+    type: "number",
+  },
+  {
+    name: "6",
+    label: 6,
+    formula: 6,
+    type: "number",
+  },
+  {
+    name: "subtract",
+    label: "-",
+    formula: "-",
+    type: "operator",
+    class: "key-operator",
+  },
+  {
+    name: "1",
+    label: 1,
+    formula: 1,
+    type: "number",
+  },
+  {
+    name: "2",
+    label: 2,
+    formula: 2,
+    type: "number",
+  },
+  {
+    name: "3",
+    label: 3,
+    formula: 3,
+    type: "number",
+  },
+  {
+    name: "add",
+    label: "+",
+    formula: "+",
+    type: "operator",
+    class: "key-operator",
+  },
+  {
+    name: "0",
+    label: 0,
+    formula: 0,
+    type: "number",
+  },
+  {
+    name: "comma",
+    label: ".",
+    formula: ".",
+    type: "number",
+  },
+  {
+    name: "equal",
+    label: "=",
+    formula: "=",
+    type: "calculate",
+    class: "key-equal",
+  },
+];
 
-//for operator display
-
-function operator(ope) {
-  isNew = true;
-  outputScreen.value += ope;
-}
-
-// for operator
-// các phép tính
-function calculate() {
-  try {
-    console.log(outputScreen.value);
-    outputScreen.value = eval(outputScreen.value);
-    console.log(outputScreen.value);
-    isNew = false;
-  } catch (err) {
-    alert("Invalid");
-  }
-}
-
-// for clear and delete
-// hàm dọn dẹp và xóa
-function Clear() {
-  outputScreen.value = "";
-  isNew = true;
-}
-
-function del() {
-  outputScreen.value = outputScreen.value.slice(0, -1);
-  isNew = true;
-}
+const createButtons = () => {
+  var added_keys = 0;
+  calculator_buttons.forEach((button, index) => {
+    keys.innerHTML += `<button id="${button.name}" class="${button.class}">${button.label}</button>`;
+    added_keys++;
+  });
+};
+createButtons();
